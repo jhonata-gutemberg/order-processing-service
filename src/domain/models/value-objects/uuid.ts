645c2ef45e12c0cf6c9986ed6c1ccd42c1d2ae8b
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-12-01 05:39:56 -0300

    implementing uuid class

---
+++ src/domain/models/value-objects/uuid.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidV4, validate as validateUUIDV4 } from "uuid";
+import { IllegalArgumentException } from "../exceptions/illegal-argument-exception";
+
+export class UUID {
+    private readonly value: string;
+
+    private constructor(value: string) {
+        if (!validateUUIDV4(value)) {
+            throw new IllegalArgumentException(`${value} is not a valid UUID`);
+        }
+        this.value = value;
+    }
+
+    public static random() {
+        return new UUID(uuidV4());
+    }
+
+    public static from(value: string) {
+        return new UUID(value);
+    }
+
+    public toString() {
+        return this.value;
+    }
+}
---
+++ test/domain/models/value-objects/uuid.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from "vitest";
+import { UUID } from "../../../../src/domain/models/value-objects/uuid";
+import { validate as validateUUIDV4 } from "uuid";
+import { IllegalArgumentException } from "../../../../src/domain/models/exceptions/illegal-argument-exception";
+
+describe("UUID", () => {
+    it("should de able to create a new random uuid", () => {
+        const uuid = UUID.random();
+
+        expect(uuid).toBeInstanceOf(UUID);
+        expect(validateUUIDV4(uuid.toString())).toBeTruthy();
+    });
+
+    it("should be able to create a new uuid from string", () => {
+        const stringUUID = "8660efc3-1064-4391-bbb0-f077f2ad161c";
+
+        const uuid = UUID.from(stringUUID);
+
+        expect(uuid).toBeInstanceOf(UUID);
+        expect(uuid.toString()).toBe(stringUUID);
+    });
+
+    it("should not be able to create a new uuid from a invalid string", () => {
+        const invalidUUID = "1234567890";
+
+        const uuid = () => UUID.from(invalidUUID);
+
+        expect(uuid).throws(
+            IllegalArgumentException,
+            `${invalidUUID} is not a valid UUID`,
+        );
+    });
+});
